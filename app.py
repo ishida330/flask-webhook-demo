d11cbd6305801005cd11764f771e7baa2738a444
Administrator <admin@example.com>
2020-01-15 11:58:24 +0900

    v1

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return '<h1>Hello World, Again!</h1>'
+    return '<h1>Hello World</h1>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
