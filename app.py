75a3ae22f6559bf0ed0a45603d50a82a10cc7e73
Administrator <admin@example.com>
2020-02-06 11:32:45 +0900

    v1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1><font color=green>Hello World!!!</font></h1>'
+    return '<h1><font color=red>Hello World!!!</font></h1>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
